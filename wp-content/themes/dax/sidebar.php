44126b0fbf62a88b2bdffd1badae8c47f256cee0
Maria Daniel Deepak <mariadanieldeepak@gmail.com>
2018-03-11 01:38:14 +0530

    Remove unused themes

--- wp-content/themes/dax/sidebar.php
+++
@@ -1,6 +0,0 @@
-
-<?php if ( is_active_sidebar( 'sidebar' )  ) : ?>
-	<aside id="secondary" class="col-md-4 sidebar widget-area rightSidebar" role="complementary">
-		<?php dynamic_sidebar( 'sidebar' ); ?>
-	</aside>
-<?php endif; ?>

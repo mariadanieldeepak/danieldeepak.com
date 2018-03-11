b7e1835c3bdbb73b22b84272567fc24b7b5f251e
Maria Daniel Deepak <mariadanieldeepak@gmail.com>
2018-03-12 00:54:55 +0530

    Include script tags

--- wp-content/themes/simplent-child/assets/js/simplent-child-tag-manager.js
+++ wp-content/themes/simplent-child/assets/js/simplent-child-tag-manager.js
@@ -1,4 +1,5 @@
 <!-- Drip -->
+<script type="text/javascript">
 var _dcq = _dcq || [];
 var _dcs = _dcs || {};
 _dcs.account = '9362997';
@@ -10,12 +11,13 @@ _dcs.account = '9362997';
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(dc, s);
 })();
+</script>
 <!-- end Drip -->
 
 <!-- Google Tag Manager -->
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TVNX528');
+})(window,document,'script','dataLayer','GTM-TVNX528');</script>
 <!-- End Google Tag Manager -->

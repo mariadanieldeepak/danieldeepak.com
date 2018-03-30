953a789ed1c8ab31ba91ae5b86904be3f1c53d0d
Maria Daniel Deepak <mariadanieldeepak@gmail.com>
2018-03-30 23:49:03 +0530

    Include Analytics only on Production

--- wp-content/themes/simplent-child/functions.php
+++ wp-content/themes/simplent-child/functions.php
@@ -3,7 +3,7 @@
 add_action( 'wp_footer', 'simplent_child_enqueue_tag_manager' );
 
 function simplent_child_enqueue_tag_manager() {
-	if ( ! is_admin() || is_user_logged_in() ) {
+	if ( ! is_user_logged_in() && simplent_child_is_production() ) {
 		include( get_stylesheet_directory() . '/assets/js/simplent-child-tag-manager.js' );
 	}
 }
@@ -15,4 +15,10 @@ function simplent_child_enqueue_styles() {
 		simplent_theme_enqueue();
 	}
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+if ( ! function_exists( 'simplent_child_is_production' ) ) {
+	function simplent_child_is_production() {
+		return (boolean) strpos( WP_SITEURL, 'danieldeepak.com' );
+	}
 }
